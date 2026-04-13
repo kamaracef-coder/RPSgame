a9970c74d44abc41d445063ddb3a9292120cedf1
kamaracef-coder <kamaracef@gmail.com>
2026-04-13 11:00:04 +0200

    begonnen aam javasript  let goed op mijn html komt niet over met filmpje

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const options = ["rock", "paper", "sizors" ];
+const playerdisplay = document.getElementById("playerdisplay")
+const computerdisplay = document.getElementById("computerdisplay")
+const resultdisplay = document.getElementById("resultdisplay")
+
+function press(playeroptions){
+    const computeroptions = options[math.floor(math.random() *3)];
+
+    console.log(computeroptions);
+}
